caf0c7e180b3c6454add8d6bd426a01df04a78c2
0xChiaChi <king565634@gmail.com>
2022-08-12 13:39:51 +0800

    new start

---
+++ js/canvas_pixel.js
@@ -0,0 +1,97 @@
+
+var canvas,board,color_pen;
+canvas = document.getElementById('imageSrc');
+canvas.width=canvas.clientWidth
+canvas.height=canvas.clientHeight
+board = canvas.getContext('2d');
+
+var mousePress = false;
+var last = null;
+//開始繪制
+function beginDraw(){
+    mousePress = true;
+}
+//繪制
+function drawing(event){
+    event.preventDefault();
+    if(!mousePress)return;
+    var xy = pos(event);
+    if(last!=null){
+        board.beginPath();
+        board.moveTo(last.x,last.y);
+        board.lineTo(xy.x,xy.y);
+        board.stroke();
+    }
+    last = xy;
+}
+//結束繪制
+function endDraw(event){
+    mousePress = false;
+    event.preventDefault();
+    last = null;
+}
+//獲取位置
+function pos(event){
+    var x,y;
+    if(isTouch(event)){
+        x = event.touches[0].pageX-event.target.offsetLeft;
+        y = event.touches[0].pageY-event.target.offsetTop;
+    }else{
+        x = event.offsetX;
+        y = event.offsetY;
+    }
+     return {x:x,y:y};
+}
+
+//檢測是touch還是mouse事件
+function isTouch(event){
+    var type = event.type;
+    if(type.indexOf('touch')>=0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//清除軌跡  
+clear.addEventListener('click',function(){
+    board.clearRect(0, 0, board.canvas.width, board.canvas.height);
+});
+// //轉換成圖片顯示
+function convertCanvasToImage() {
+    var image = canvas.toDataURL("image/png"); 
+    $('#image').html("<img src='"+image+"' alt='from canvas'/>");
+}
+
+//生成圖片並上傳到服務器
+function UploadPic() {       
+    var Pic = canvas.toDataURL("image/png");
+    Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "")
+ 
+    $.ajax({
+        type: 'POST',
+        url: 'url',
+        data: '{ "imageData" : "' + Pic + '" }',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (msg) {
+            console.log("圖片上傳成功");
+        },
+        error: function(msg) {
+            alert("需要服務器資源");
+        }
+    });
+}        
+tooltip.onchange= function (event){
+    color_pen=event.target.value;
+    board.strokeStyle=color_pen;
+ }
+
+board.lineWidth = 3;
+board.lineJoin = "round";
+canvas.onmousedown = beginDraw;
+canvas.onmousemove = drawing;
+canvas.onmouseup = endDraw;
+canvas.addEventListener('touchstart',beginDraw,false);
+canvas.addEventListener('touchmove',drawing,false);
+canvas.addEventListener('touchend',endDraw,false);
---
+++ js/downloadbutton.js
@@ -0,0 +1,66 @@
+// 下載按鈕
+var downloadbutton = document.getElementById("downloadbutton");
+
+downloadbutton.addEventListener("click", function(){
+    if(checkEmpty(1)!=true){
+    var canvas = document.getElementById('imageSrc');
+    var anchor = document.createElement('a');
+    anchor.href = canvas.toDataURL("image/png");
+    anchor.download = "imageSrc.png";
+    anchor.click();
+    }
+// })
+
+// downloadbutton.addEventListener("click", function(){
+//     if(checkEmpty(2)!=true){
+//     var canvas = document.getElementById('Share1Layer');
+//     var anchor = document.createElement('a');
+//     anchor.href = canvas.toDataURL("image/png");
+//     anchor.download = "Share1Layer.png";
+//     anchor.click();
+//     }
+// })
+
+// downloadbutton.addEventListener("click", function(){
+//     if(checkEmpty(2)!=true){
+//     var canvas = document.getElementById('Share2Layer');
+//     var anchor = document.createElement('a');
+//     anchor.href = canvas.toDataURL("image/png");
+//     anchor.download = "Share2Layer.png";
+//     anchor.click();
+//     }
+// })
+
+// downloadbutton.addEventListener("click", function(){
+//     if(checkEmpty(2)!=true){
+//     var canvas = document.getElementById('verificLayer');
+//     var anchor = document.createElement('a');
+//     anchor.href = canvas.toDataURL("image/png");
+//     anchor.download = "verificLayer.png";
+//     anchor.click();
+//     }
+// })
+
+//驗證canvas畫布是否為空函數
+    function isCanvasBlank(checkcanvas) {
+    var blank = document.createElement('canvas');//系統獲取一個空canvas對象
+    blank.width = checkcanvas.width;
+    blank.height = checkcanvas.height;
+    return checkcanvas.toDataURL() == blank.toDataURL();//比較值相等則為空
+}
+//調用
+//表單非空驗證
+function checkEmpty(x) {
+    var check=document.getElementById("imageSrc"); // 獲取html的canvas對象，我這個id="imageSrc"
+    // var check2=document.getElementById("Share1Layer"); // 獲取html的canvas對象，我這個id="Share1Layer"
+    switch(x){
+    	case 1: if(isCanvasBlank(check)){
+        		return true;
+        		}
+        		break;
+       //  case 2: if(isCanvasBlank(check2)){
+       //  		return true;
+   				// }
+   				// break;
+    }
+}
