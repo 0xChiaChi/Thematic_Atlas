aef88e610dba37576faae542a88388b86685ce96
0xChiaChi <king565634@gmail.com>
2023-03-27 14:51:33 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,11 +6,11 @@
  define('PORT','63306');
 // 建立MySQL的資料庫連接 
 $link = mysqli_connect(SERVER,USERNAME,PASSWORD,NAME,PORT); 
-mysqli_q
-if ( !$l
-   die("
+mysqli_query($link, 'SET NAMES utf8');
+if ( !$link ) {
+   die("MySQL資料庫連接錯誤!" . mysqli_connect_error());
 }
 else {
-   retur
+   return $link;
 }
 ?>	
