937bf9392ef413730e9609dc64f47d3827af1b63
0xChiaChi <king565634@gmail.com>
2023-03-27 14:45:23 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
- define('SERVER','hub.kuaz.info');
+ define('SERVER','hsinchuforti.kuaz.dev');
  define('USERNAME','holo_chia77');
  define('PASSWORD','gura_and_mumei');
  define('NAME','Thematic_Atlas');
  define('PORT','63306');
 // 建立MySQL的資料庫連接 
 $link = mysqli_connect(SERVER,USERNAME,PASSWORD,NAME,PORT); 
-mysqli_query($link, 'SET NAMES utf8');
-if ( !$link ) {
-   die("MySQL資料庫連接錯誤!" . mysqli_connect_error());
+mysqli_q
+if ( !$l
+   die("
 }
 else {
-   return $link;
+   retur
 }
 ?>	
